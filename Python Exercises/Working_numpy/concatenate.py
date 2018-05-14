9f43f30e950524ffca36b4275d48e4912ca05771
basilwong <wong.bas@gmail.com>
2018-05-14 11:23:51 -0700

    Finished more challenges.

---
+++ Python Exercises/Working_numpy/concatenate.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+"""
+4 3 2
+1 2
+1 2
+1 2
+1 2
+3 4
+3 4
+3 4
+"""
+
+measurements = np.array(input().strip().split( ), int)
+
+N = measurements[0]
+M = measurements[1]
+P = measurements[2]
+
+N_rows =
+for i in range(N):
+    
