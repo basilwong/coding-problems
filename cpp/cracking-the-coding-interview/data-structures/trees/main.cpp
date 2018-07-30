630e6ab137ed961a733c1a8a2f9f427278c3f44f
basilwong <wong.bas@gmail.com>
2018-07-29 23:11:57 -0700

    Started on binary search tree checking.

--- cpp/cracking-the-coding-interview/data-structures/trees/main.cpp
+++ cpp/cracking-the-coding-interview/data-structures/trees/main.cpp
@@ -1,6 +1,57 @@
-#include <iostream>
+/*
+ * The below code checks whether the input tree is a binary search tree.
+ */
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
 }
+
+// Checks if the left child is lower than the parent. Additionally, it calls
+// check_right and itself recursively to check the child's children.
+bool check_left(Node *root) {
+  bool compare;
+  bool left;
+  bool right;
+  Node* left_child;
+  Node* child_of_child;
+
+  // If the node has no left child, return true. This part of the binary tree is
+  // within bst parameters.
+  if (root->left == NULL) {
+    return true;
+  }
+
+  // Instantiate node of the left child.
+  left_child = root->left;
+
+  // Compare the date of the left child with the data of the parent.
+  if (root->data > left_child->data) {
+
+  }
+
+}
+
+// Checks if the right child is lower than the parent. Additionally, it calls
+// check_left and itself recursively to check the child's children.
+bool check_right(Node *root) {
+
+}
+
+bool checkBST(Node *root) {
+  if ((check_left(root)) && (check_right(root))) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+
+//
+//int main() {
+//  std::cout << "Hello, World!" << std::endl;
+//  return 0;
+//}
