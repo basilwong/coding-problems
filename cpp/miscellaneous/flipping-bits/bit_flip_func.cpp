856b8e8f1aec9b63bd132c4414ff5055a05ebae7
basilwong <wong.bas@gmail.com>
2019-03-08 12:05:06 -0800

    small snippet

---
+++ cpp/miscellaneous/flipping-bits/bit_flip_func.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Function for flipping the bit of a long variable. 
+long flippingBits(long n) {
+  return  n ^ UINT_MAX;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        long n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        long result = flippingBits(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
