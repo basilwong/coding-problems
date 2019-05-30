7007b0667bb3de8ed226c8aa0a8bb5be28637de0
Chelsea Tang <chelseatang@Chelseas-MBP.hitronhub.home>
2019-05-30 00:51:33 -0700

    7/16 tests passed

---
+++ python/medium/algorithms/graph-theory/journey_to_the_moon.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the journeyToMoon function below.
+def journeyToMoon(n, astronaut):
+    
+    #construct 2d array of 
+    pairs = list()
+
+    for _ in range(n):
+        pairs.append(set())
+
+    for dude in astronaut:
+        pairs[dude[0]].add(dude[1])
+        pairs[dude[1]].add(dude[0])
+
+    # Visited set 
+    visited = set()
+
+    # Number of possible pairs
+    sum = 0
+
+    for i in range(n):
+        if i not in visited:
+            visited.add(i)
+            visited.update(pairs[i])
+            sum += (len(pairs[i]) + 1) * (n - len(pairs[i]) - 1)
+
+    return int(sum / 2)
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    np = input().split()
+
+    n = int(np[0])
+
+    p = int(np[1])
+
+    astronaut = []
+
+    for _ in range(p):
+        astronaut.append(list(map(int, input().rstrip().split())))
+
+    result = journeyToMoon(n, astronaut)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
