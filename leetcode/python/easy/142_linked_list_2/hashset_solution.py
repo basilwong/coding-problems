f87241aca833da48f603a24ef313f0cebe7c7db2
basilwong <wong.bas@gmail.com>
2019-11-17 15:11:26 -0800

    linked list cycle 2

---
+++ leetcode/python/easy/142_linked_list_2/hashset_solution.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+
+        if not head:
+            return None
+
+        while head.next:
+
+            head.val = str(head.val)
+
+            head = head.next
+
+            if type(head.val) is str:
+                head.val = int(head.val)
+                return head
+
+        return None
